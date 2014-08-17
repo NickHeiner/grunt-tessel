bd36f09ced1f08b10a24380c08cd77dabc4aa033
Nick Heiner <nick.heiner@opower.com>
2014-08-16 20:38:24 -0400

    adding task descriptions

--- tasks/grunt-tessel-push.js
+++ tasks/grunt-tessel-push.js
@@ -4,7 +4,10 @@ var _ = require('lodash');
 
 module.exports = function(grunt) {
 
-    grunt.registerMultiTask('tessel-push', function() {
+    grunt.registerMultiTask('tessel-push', 'Runs `tessel push` to deploy to tessel. ' +
+        'The code will be written into flash memory. This memory will wear out after ' +
+        'many uses, so you should use `tessel-run` instead during development, and only ' +
+        'run this when you want to deploy more permanently.', function() {
         var done = this.async(),
             options = this.options({
                 log: grunt.log.ok,
@@ -17,7 +20,9 @@ module.exports = function(grunt) {
         });
     });
 
-    grunt.registerMultiTask('tessel-run', function() {
+    grunt.registerMultiTask('tessel-run', 'Runs `tessel run` to deploy to tessel. ' +
+        'The script will be run but not written into flash memory. ' +
+        'This should be your default while developing.', function() {
         var done = this.async(),
             options = this.options({
                 log: grunt.log.ok,
